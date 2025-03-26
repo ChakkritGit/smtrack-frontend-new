fd60e2a7048a1617e4a3cc102bf257cd6b94c089
ChakkritGit <nongtonnee@gmail.com>
2025-03-26 13:53:59 +0700

    fix: comment out translation logic in Loading component for cleaner code

--- src/components/skeleton/table/loading.tsx
+++ src/components/skeleton/table/loading.tsx
@@ -1,12 +1,12 @@
-import { useTranslation } from 'react-i18next'
+// import { useTranslation } from 'react-i18next'
 
 const Loading = () => {
-  const { t } = useTranslation()
+  // const { t } = useTranslation()
 
   return (
     <div className='flex items-center justify-center w-full p-3 h-full gap-3'>
       <span className='loading loading-spinner loading-md bg-base-content'></span>
-      <span className='text-base-content'>{t('loading')}</span>
+      {/* <span className='text-base-content'>{t('loading')}</span> */}
     </div>
   )
 }
